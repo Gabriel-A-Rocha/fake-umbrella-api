8d0432acddc3c4baaa17669474850f57e67ef5c2
Gabriel Rocha <gabriel.altair.rocha@gmail.com>
2022-02-16 00:37:19 -0500

    remove example files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CustomersModule } from './customers/customers.module';
 
 @Module({
   imports: [CustomersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
