a1ea027709d1097ea807b6dc9a679bbf4815de25
Gabriel Rocha <gabriel.altair.rocha@gmail.com>
2022-02-16 21:38:15 -0500

    create weather forecast service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CustomersModule } from './customers/customers.module';
+import { WeatherForecastService } from './weather-forecast/weather-forecast.service';
 
 @Module({
   imports: [CustomersModule],
   controllers: [],
-  providers: [],
+  providers: [WeatherForecastService],
 })
 export class AppModule {}
